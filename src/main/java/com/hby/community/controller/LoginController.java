e47fff406a7d6482d17871998734155598de98a3
misakam831 <1162157581@qq.com>
2021-04-10 16:38:20 +0800

    退出与过滤器功能

--- src/main/java/com/hby/community/controller/LoginController.java
+++ src/main/java/com/hby/community/controller/LoginController.java
@@ -136,7 +136,7 @@ public class LoginController implements CommunityConstant {
     @RequestMapping(path = "/logout",method = RequestMethod.GET)
     public String logout(@CookieValue("ticket") String ticket){
         userService.logout(ticket);
-        return "redirect:/login";
+        return "/site/login";
     }
 
 }
