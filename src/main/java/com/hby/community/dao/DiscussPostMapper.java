f3fa061cd942c16dbef19cce63c4ac1538ed3d85
misakam831 <1162157581@qq.com>
2021-04-26 10:13:57 +0800

    查看评论

--- src/main/java/com/hby/community/dao/DiscussPostMapper.java
+++ src/main/java/com/hby/community/dao/DiscussPostMapper.java
@@ -12,4 +12,7 @@ public interface DiscussPostMapper {
 
     int selectDiscussPostRows(@Param("userId") int userId);
 
+    int insertDiscussPost(DiscussPost discussPost);
+
+    DiscussPost selectDiscussPostById(int id);
 }
--- src/main/java/com/hby/community/service/DiscussPostService.java
+++ src/main/java/com/hby/community/service/DiscussPostService.java
@@ -2,8 +2,10 @@ package com.hby.community.service;
 
 import com.hby.community.dao.DiscussPostMapper;
 import com.hby.community.entity.DiscussPost;
+import com.hby.community.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -11,6 +13,8 @@ import java.util.List;
 public class DiscussPostService {
     @Autowired
     private DiscussPostMapper discussPostMapper;
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
 
     public List<DiscussPost> findDiscussPost(int userId,int offset,int limit){
         return discussPostMapper.selectDiscussPosts(userId, offset, limit);
@@ -19,5 +23,25 @@ public class DiscussPostService {
     public int findDiscussPostsRows(int userId){
         return discussPostMapper.selectDiscussPostRows(userId);
     }
+
+    public int addDisscussPost(DiscussPost post){
+        if (post==null){
+            throw new IllegalArgumentException("必须添加讨论帖");
+        }
+
+        //转义html
+        post.setTitle(HtmlUtils.htmlEscape(post.getTitle()));
+        post.setContent(HtmlUtils.htmlEscape(post.getContent()));
+        //过滤
+        post.setTitle(sensitiveFilter.filter(post.getTitle()));
+        post.setContent(sensitiveFilter.filter(post.getContent()));
+
+        return discussPostMapper.insertDiscussPost(post);
+    }
+
+    public DiscussPost findDiscussPostById(int id){
+        return discussPostMapper.selectDiscussPostById(id);
+    }
+
 
 }
